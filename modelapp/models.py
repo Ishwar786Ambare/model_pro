8e13b7b0f2dd8385c80defada235189383f0f43f
NextClick <ishwarambare@gmail.com>
2020-06-24 11:28:18 +0530

    new1

--- modelapp/models.py
+++ modelapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from datetime import datetime as dt
 
+
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     address = models.CharField(max_length=100)
-
