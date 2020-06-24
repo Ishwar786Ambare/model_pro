37c0682faa498d1a592f9d7951418998b1d3a683
NextClick <ishwarambare@gmail.com>
2020-06-24 14:52:11 +0530

    last change before lunchimage,static and media root all set

--- modelapp/models.py
+++ modelapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    mobile = models.BigIntegerField()
+    email = models.EmailField(max_length=100, default="customer@gmail.com")
+    mobile = models.BigIntegerField(default=9066371333)
     # password = models.PasswordField(max_length=100)
     address = models.CharField(max_length=100)
 
