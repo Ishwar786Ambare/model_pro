6205dc8f2e510edf292e6f1e3ed7a17e7b02ab19
NextClick <ishwarambare@gmail.com>
2020-06-24 13:02:27 +0530

    last change before lunchimage,static and media root all set

--- modelapp/models.py
+++ modelapp/models.py
@@ -2,14 +2,20 @@ from django.db import models
 from datetime import datetime as dt
 
 
+
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
+    mobile = models.BigIntegerField()
+    # password = models.PasswordField(max_length=100)
     address = models.CharField(max_length=100)
 
 
 class Vendor(models.Model):
-    pass
+    name = models.CharField(max_length=100, blank=True, null=True, default='vendor')
+    mobile = models.BigIntegerField()
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
 
 
 class Products(models.Model):
@@ -19,10 +25,6 @@ class Products(models.Model):
     description = models.TextField(blank=True, null=True)
     stock = models.BigIntegerField()
     image = models.ImageField(null=True, blank=True, upload_to="media/")
-    
-
 
     def __str__(self):
         return self.name
-
-
