70c27b80131fdc9f56fee11647801d1226787c84
NextClick <ishwarambare@gmail.com>
2020-06-24 11:36:16 +0530

    new1

--- modelapp/models.py
+++ modelapp/models.py
@@ -6,3 +6,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     address = models.CharField(max_length=100)
+
+
+class Vendor(models.Model):
+    pass
