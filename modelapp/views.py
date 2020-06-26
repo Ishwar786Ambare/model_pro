b700a2771148eff630004d9b9d1a8ed0669e8a1b
NextClick <ishwarambare@gmail.com>
2020-06-26 15:29:05 +0530

    new day work

--- modelapp/views.py
+++ modelapp/views.py
@@ -22,8 +22,23 @@ def current_page(request):
             user = form.cleaned_data['user']
             mobile = form.cleaned_data['mobile']
             address = form.cleaned_data['address']
-            print(name)
-            send_mail("Hi"+name+"this is from NEXTCLICK", "your" + order + "is ready for dispatch\nthank you for your shopping|||", "ambareishu@gmail.com",
+
+            data = Customer(
+                name=name,
+                order=order,
+                email=email,
+                user=user,
+                mobile=mobile,
+                address=address,
+            )
+            data.save()
+            a=data.author = request.user
+            print(a)
+
+            qu = Customer.objects.all()
+            print(qu.name)
+            send_mail("Hi" + qu.name + "this is from NEXTCLICK",
+                      "your" + qu.email + "is ready for dispatch\nthank you for your shopping|||", "ambareishu@gmail.com",
                       ['ishwarambare@hotmail.com', 'ishwarambare@gmail.com'], fail_silently=False)
             form.save()
             redirect(mailsend)
@@ -33,8 +48,10 @@ def current_page(request):
 
 
 def mailsend(request):
-    send_mail("this is from NEXTCLICK", "your car is ready for dispatch\nthank you for your shopping", "ambareishu@gmail.com",
-              ['ishwarambare@hotmail.com', 'ishwarambare@gmail.com'], fail_silently=False)
+    # send_mail("this is from NEXTCLICK", "your car is ready for dispatch\nthank you for your shopping",
+    #           "ambareishu@gmail.com",
+    #           ['ishwarambare@hotmail.com', 'ishwarambare@gmail.com'], fail_silently=False)
+    # data = Customer.objects.filter(email=ishwarambre@gmil.com)
 
     return HttpResponse("mail_will_be_send_check_your_mail_please_thank_you_for_shopping_have_a_good_day_ahead")
-    # return render(request, 'mailsend.html', {'data': data})
+    # return render(request, 'mailsend.html', {'data':data })
